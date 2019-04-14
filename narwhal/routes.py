6840a0115e23ce5c3a284feef9da80ead679c078
Kevin Higgs <github@kevinhiggs.com>
2019-04-14 14:20:32 -0400

    Revert "Merge branch 'master' of github.com:blairsec/narwhal"
    This reverts commit 681628abdeb2e8b625506775543216be0117ad25, reversing
    changes made to 7ed11cf65005cb3323cea5a3555065e77267b834.

--- narwhal/routes.py
+++ narwhal/routes.py
@@ -6,7 +6,7 @@ from . import utils
 
 def load_stream(req):
 	try:
-		return json.loads(str(req.stream.read().decode("utf-8")))
+		return json.load(req.stream)
 	except:
 		raise falcon.HTTPBadRequest('Bad request', 'Invalid JSON data.')
 
@@ -32,7 +32,7 @@ class Instances(object):
 		tag = data.get('tag', 'latest')
 		options = data.get('options', {})
 		try:
-			resp.body = utils.create_instance(repo, tag, options).name
+			utils.create_instance(repo, tag, options)
 		except Exception as e:
 			raise falcon.HTTPInternalServerError('Internal server error', str(e))
 
@@ -59,7 +59,6 @@ class Instance(object):
 		utils.remove_instance(container)
 
 	def handle_action(self, container, action):
-		utils.update_proxy_networks(container)
 		if action == 'start':
 			container.start()
 		elif action == 'stop':
